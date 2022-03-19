bd455ecb2825b4162f3e199f3997bae06f2b1a6c
ginying <>
2022-03-19 21:58:18 +0700

    Add duplicated word checking

--- src/components/AddWord/AddWord.js
+++ src/components/AddWord/AddWord.js
@@ -2,27 +2,47 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./AddWord.css";
 import db from "../../firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
 
 const AddWord = () => {
   const bannedWords = "banned-words";
   const [word, setWord] = useState("");
+  const [emptyWordSnapshot, setEmptyWordSnapshot] = useState(true);
+
+  const checkEmptyWordSnapshot = async () => {
+    const queryRef = query(
+      collection(db, bannedWords),
+      where("word", "==", word)
+    );
+    const querySnapshot = await getDocs(queryRef);
+    setEmptyWordSnapshot(querySnapshot.empty);
+    console.log(word, querySnapshot.empty);
+    return querySnapshot.empty;
+  };
 
   const addWord = async (e) => {
-    console.log(word);
-    if (sessionStorage.getItem("playerName")==="host") {
+    if (word && await checkEmptyWordSnapshot()) {
       e.preventDefault();
       await setBannedWord(word);
-
       setWord("");
     }
   };
 
-  const setBannedWord = (word) => {
+  const setBannedWord = async (word) => {
     const bannedWordsRef = collection(db, bannedWords);
-    return addDoc(bannedWordsRef, {
+    await addDoc(bannedWordsRef, {
       word: word,
     });
+  };
+
+  const ErrorMessage = () => {
+    if (!emptyWordSnapshot && word)
+      return (
+        <div className="pt-1 text-sm text-rose-800">
+          Duplicate word!
+        </div>
+      );
+    else return <div className="hidden"></div>;
   };
 
   return (
@@ -38,6 +58,7 @@ const AddWord = () => {
             className="block w-full py-2 px-3 sm:text-sm border border-gray-300 rounded-md text-center"
           ></input>
         </div>
+        <ErrorMessage></ErrorMessage>
       </div>
       <button
         onClick={addWord}
